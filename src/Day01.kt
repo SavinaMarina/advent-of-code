c9e06dbfcdbfce00e04c33d51fbd07c7b84d6236
Marina <savina.marina@yahoo.com>
2021-12-02 12:46:29 +0300

    Day 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+	    var prev = 0
+        var cnt = 0
+        for (v in input) {
+            if (v > prev && prev != 0) cnt++
+            prev = v
+        }
+        return cnt
     }
+	
+	fun part1f(input: List<Int>): Int {
+		return input.windowed(2).count { (x,y) -> y > x}
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+		var prevSum = 0
+        var sum = 0
+        var cnt = 0
+        for (i in input.indices) {
+            if (i >= 2)  {
+                sum = input[i-2] + input[i-1] + input[i]
+                if ((prevSum != 0) && (sum > prevSum)) {
+                    cnt++
+                }
+                prevSum = sum
+            }
+        }
+        return cnt
     }
+	
+	fun part2f(input: List<Int>): Int {
+        return input.windowed(3) { it.sum() }
+            .windowed(2).count { (x, y) -> y > x }
+	}
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
---
+++ src/Day02.kt
@@ -0,0 +1,72 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var x = 0
+        var y = 0
+        for (v in input) {
+            val (direction, step) = v.split(" ")
+            val stepValue = step.toInt()
+            when (direction ){
+                "forward" -> x += stepValue
+                "down" -> y += stepValue
+                "up" -> y -= stepValue
+            }
+        }
+        return x * y
+    }
+
+    fun part1f(input: List<String>): Int {
+        val (x, y) = input.map { val (direction, step) = it.split(" ")
+                    val stepValue = step.toInt()
+                    when (direction ){
+                        "forward" -> Pair(stepValue, 0)
+                        "down" -> Pair(0, stepValue)
+                        "up" -> Pair(0, -stepValue)
+                        else -> Pair(0,0)
+                    }
+        }.reduce { acc, v -> Pair(acc.first + v.first, acc.second + v.second) }
+        return x * y
+    }
+
+    fun part2(input: List<String>): Int {
+        var aim = 0
+        var x = 0
+        var y = 0
+        for (v in input) {
+            val (direction, step) = v.split(" ")
+            val stepValue = step.toInt()
+            when (direction ){
+                "forward" -> {
+                    x += stepValue
+                    y += aim*stepValue
+                }
+                "down" -> aim += stepValue
+                "up" -> aim -= stepValue
+            }
+        }
+        return x * y
+    }
+
+    fun part2f(input: List<String>): Int {
+        val (x, y, aim) = input.map { val (direction, step) = it.split(" ")
+            val stepValue = step.toInt()
+            when (direction ){
+                "forward" -> Triple(stepValue,  0, 0)
+                "down" -> Triple(0,  0, stepValue)
+                "up" -> Triple(0,  0, -stepValue)
+                else -> Triple(0, 0, 0)
+            }
+        }.reduce { acc, v ->
+            Triple(acc.first + v.first, acc.second + v.first * acc.third, acc.third + v.third) }
+        return x * y
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+    check(part2f(testInput) == 900)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
